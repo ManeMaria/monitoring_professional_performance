627644d7ba635fd0b2ab88fc75728555284c004e
ManeMaria <mirandacesar598@gmail.com>
2025-11-12 21:58:24 -0300

    chore: modify date values for iso string

--- src/modules/panel/data/initial-month-progress.ts
+++ src/modules/panel/data/initial-month-progress.ts
@@ -6,17 +6,20 @@ export const initialMonthProgress: MonthProgress[] = [
 		focus: "Setup e fundamentos de testes",
 		progress: 0,
 		notes: "",
+		date: "2025-11-30T00:00:00",
 	},
 	{
 		month: "Dezembro/25",
 		focus: "Cobertura e documentação técnica",
 		progress: 0,
 		notes: "",
+		date: "2025-12-31T00:00:00",
 	},
 	{
 		month: "Janeiro/26",
 		focus: "Consolidação e compartilhamento",
 		progress: 0,
 		notes: "",
+		date: "2026-01-31T00:00:00",
 	},
 ];
